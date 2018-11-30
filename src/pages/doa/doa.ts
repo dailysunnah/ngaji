18fbf58f915aefb5341e6e9db14e8ddc9ed79aaa
pujie <puji@gmail.com>
2018-11-30 16:59:56 +0700

    add content to doa

--- src/pages/doa/doa.ts
+++ src/pages/doa/doa.ts
@@ -14,10 +14,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'doa.html',
 })
 export class DoaPage {
-
+  items: Array<{title: string, note: string, icon: string}>;
+  icons:string[]
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
 
+    this.items = [
+      {title:'Do-a Pagi',note:'Do-a Pagi Hari',icon:'0'},
+      {title:'Do-a Sore',note:'Do-a Sore Hari',icon:'1'}
+    ];
+  }
+  itemTapped(event, item) {
+    // That's right, we're pushing to ourselves!
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad DoaPage');
   }
